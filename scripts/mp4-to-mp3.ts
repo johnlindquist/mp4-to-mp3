ed2e6379252a04f0464937de06d508c9956f0b4b
John Lindquist <johnlindquist@gmail.com>
2023-01-30 16:27:05 -0700

    fix(mp4s): convert all mp4s from latest commit

--- scripts/mp4-to-mp3.ts
+++ scripts/mp4-to-mp3.ts
@@ -7,59 +7,64 @@ console.log(`After import...`)
 console.log(`KENV: ${process.env.KENV}`)
 
 // get the most recently added file in the current directory that is not a directory
-let { stdout: file } = await $`ls -t $(find . -maxdepth 1 -type f -name "*.mp4") | head -1`
+let { stdout: files } = await $`git show HEAD --name-only --pretty="" | grep ".mp4$"`
 
-console.log({ file })
+// convert files into an array
+let filesArray = files.split(`\n`).filter(Boolean)
 
-file = file?.trim()
+let tag_name = `mp3s`
 
-if (!file) {
-  console.log(`No mp4 found. Exiting early`)
-  exit(0)
-}
-
-let { name } = path.parse(file)
-let output = `${name}.mp3`
-
-console.log(`Converting ${file} to ${output}`)
-
-// convert the file to mp3
-await $`ffmpeg -i ${file} ${output}`
-
-console.log(`After ffmpeg...`)
-
-// TODO: Add types for github actions to kit repo
 declare const github: any
+declare const octokit: any
 
-let octokit = github.getOctokit(await env("GITHUB_TOKEN"))
-
-console.log(`After octokit...`)
-
-let tag_name = name
-
-let releaseResponse = await octokit.rest.repos.createRelease({
+// Check to see if the release already exists
+let releaseResponse = await octokit.rest.repos.getReleaseByTag({
   ...github.context.repo,
-  tag_name,
-  name: tag_name,
+  tag: tag_name,
 })
 
-console.log(`After releaseResponse...`)
-
-let uploadResponse = await octokit.rest.repos.uploadReleaseAsset({
-  ...github.context.repo,
-  release_id: releaseResponse.data.id,
-  name: output,
-  data: await readFile(output),
-})
-
-console.log(`After uploadResponse...`)
-
-try {
-  console.log(`Uploaded ${output} to ${uploadResponse.data.browser_download_url}`)
-} catch (error) {
-  console.log(error)
+// If the release doesn't exist, create it
+if (!releaseResponse?.data?.id) {
+  releaseResponse = await octokit.rest.repos.createRelease({
+    ...github.context.repo,
+    tag_name,
+    name: tag_name,
+  })
 }
 
-let url = `https://github.com/johnlindquist/kitapp/releases/download/${tag_name}/${output}`
+for await (let file of filesArray) {
+  let { name } = path.parse(file)
+  let output = `${name}.mp3`
 
-console.log({ url })
+  console.log(`Converting ${file} to ${output}`)
+
+  // convert the file to mp3
+  await $`ffmpeg -i ${file} ${output}`
+
+  console.log(`After ffmpeg...`)
+
+  let octokit = github.getOctokit(await env("GITHUB_TOKEN"))
+
+  console.log(`After octokit...`)
+
+  console.log(`After releaseResponse...`)
+
+  let uploadResponse = await octokit.rest.repos.uploadReleaseAsset({
+    ...github.context.repo,
+    release_id: releaseResponse.data.id,
+    name: output,
+    data: await readFile(output),
+  })
+
+  console.log(`After uploadResponse...`)
+
+  try {
+    console.log(`Uploaded ${output} to ${uploadResponse.data.browser_download_url}`)
+  } catch (error) {
+    console.log(error)
+  }
+
+  let url = `https://github.com/johnlindquist/kitapp/releases/download/${tag_name}/${output}`
+
+  console.log({ url })
+}
